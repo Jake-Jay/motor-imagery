1ee809ac90380f454eb4f0f9b6bf5ffc9e02291b
alexandralin528 <sandralin0528@gmail.com>
2021-05-05 22:27:18 +0200

    offline processing structure

--- preprocessing/Preprocessing.py
+++ preprocessing/Preprocessing.py
@@ -1,6 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from util.Dataloader import Dataloader
+
+if __name__ == "__main__" and __package__ is None:
+    from sys import path
+    from os.path import dirname as dir
+
+    path.append(dir(path[0]))
+    __package__ = "preprocessing"
+
+import util.Dataloader
+# from util.Dataloader import Dataloader
 
 
 class Preprocessing:
