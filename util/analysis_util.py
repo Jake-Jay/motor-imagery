1c422c281e43ec720cd3fec699c45b19e1c8e262
Jake Pencharz <jake.jay.p@gmail.com>
2021-05-10 00:08:40 +0200

    Implement full pipeline

---
+++ util/analysis_util.py
@@ -0,0 +1,146 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import mlab
+import matplotlib.transforms as mtransforms
+
+
+def psd(trials, nchannels, nsamples, fs):
+    '''
+    Calculates for each trial the Power Spectral Density (PSD).
+    
+    Parameters
+    ----------
+    trials : 3d-array (channels x samples x trials)
+        The EEG signal
+    
+    Returns
+    -------
+    trial_PSD : 3d-array (channels x PSD x trials)
+        the PSD for each trial.  
+    freqs : list of floats
+        Yhe frequencies for which the PSD was computed (useful for plotting later)
+    '''
+    
+    ntrials = trials.shape[2]
+    trials_PSD = np.zeros((nchannels, (nsamples//2) + 1, ntrials)) #changed to 1001
+
+    # Iterate over trials and channels
+    for trial in range(ntrials):
+        for ch in range(nchannels):
+            # Calculate the PSD
+            (PSD, freqs) = mlab.psd(trials[ch,:,trial], NFFT=int(nsamples), Fs=fs)
+            trials_PSD[ch, :, trial] = PSD.ravel()
+                
+    return trials_PSD, freqs
+
+
+
+def plot_psd(trials_PSD, freqs, chan_ind, classes:list, chan_lab=None, maxy=None):
+    '''
+    Plots PSD data calculated with psd().
+    
+    Parameters
+    ----------
+    trials : 3d-array
+        The PSD data, as returned by psd()
+    freqs : list of floats
+        The frequencies for which the PSD is defined, as returned by psd() 
+    chan_ind : list of integers
+        The indices of the channels to plot
+    chan_lab : list of strings
+        (optional) List of names for each channel
+    maxy : float
+        (optional) Limit the y-axis to this value
+    '''
+    plt.figure(figsize=(12,5))
+    
+    nchans = len(chan_ind)
+    
+    # Maximum of 3 plots per row
+    nrows = int(np.ceil(nchans / 3))
+    ncols = min(3, nchans)
+    
+    # Enumerate over the channels
+    for i,ch in enumerate(chan_ind):
+        # Figure out which subplot to draw to
+        ax = plt.subplot(nrows,ncols,i+1)
+    
+        # Plot the PSD for each class
+        for cl in trials_PSD.keys():
+            plt.plot(freqs, np.mean(trials_PSD[cl][ch,:,:], axis=1), label=cl)
+    
+        # All plot decoration below...
+
+        trans = mtransforms.blended_transform_factory(ax.transData, ax.transAxes)
+        ax.fill_between(freqs, 0, 1, where=np.logical_and(freqs > 8, freqs < 16),
+                facecolor='green', alpha=0.2, transform=trans, label='Alpha Band')
+
+        plt.xlim(1,30)
+        
+        if maxy != None:
+            plt.ylim(0,maxy)
+    
+        plt.grid()
+    
+        plt.xlabel('Frequency (Hz)')
+        
+        if chan_lab == None:
+            plt.title('Channel %d' % (ch+1))
+        else:
+            plt.title(chan_lab[i])
+
+        plt.legend()
+        
+    plt.tight_layout()
+
+
+# Calculate the log(var) of the trials
+def logvar(trials):
+    '''
+    Calculate the log-var of each channel.
+    
+    Parameters
+    ----------
+    trials : 3d-array (channels x samples x trials)
+        The EEG signal.
+        
+    Returns
+    -------
+    logvar - 2d-array (channels x trials)
+        For each channel the logvar of the signal
+    '''
+    return np.log(np.var(trials, axis=1))
+
+def plot_logvar(trials, nchannels, cl_labels):
+    '''
+    Plots the log-var of each channel/component.
+    arguments:
+        trials - Dictionary containing the trials (log-vars x trials) for 2 classes.
+    '''
+    plt.figure(figsize=(12,5))
+    
+    x0 = np.arange(nchannels)
+    x1 = np.arange(nchannels) + 0.4
+
+    y0 = np.mean(trials[cl_labels[0]], axis=1)
+    y1 = np.mean(trials[cl_labels[1]], axis=1)
+
+    plt.bar(x0, y0, width=0.5, color='b')
+    plt.bar(x1, y1, width=0.4, color='r')
+
+    plt.xlim(-0.5, nchannels+0.5)
+
+    plt.gca().yaxis.grid(True)
+    plt.title('log-var of each channel/component')
+    plt.xlabel('channels/components')
+    plt.ylabel('log-var')
+    plt.legend(cl_labels)
+
+# Scatter plot
+def plot_scatter(left, foot, cl_lab):
+    plt.figure()
+    plt.scatter(left[0,:], left[-1,:], color='b')
+    plt.scatter(foot[0,:], foot[-1,:], color='r')
+    plt.xlabel('Last component')
+    plt.ylabel('First component')
+    plt.legend(cl_lab)
--- util/preprocess_util.py
+++ util/preprocess_util.py
@@ -1,14 +1,15 @@
 from scipy import signal
 import numpy as np
+from numpy import linalg
 import matplotlib.pyplot as plt
 import mne
 import pdb
 
 
+# -----------------------------------------------------------------------
+# Wrapper Functions 
+# -----------------------------------------------------------------------
 
-# -----------------------------------------------------------------------
-# Preprocessing 
-# -----------------------------------------------------------------------
 
 def preprocess(samples_buffer, use_mne=False, **kwargs):
     '''All the preprocessing and filtering
@@ -51,6 +52,10 @@ def preprocess(samples_buffer, use_mne=False, **kwargs):
     return preprocessed_buffer
 
 
+# -----------------------------------------------------------------------
+# Standardize and reduce noise
+# -----------------------------------------------------------------------
+
 def rereference(samples_buffer, channel=None):
     '''Rerefence all data wrt channel
     '''
@@ -59,6 +64,13 @@ def rereference(samples_buffer, channel=None):
 
     return samples_buffer - samples_buffer[channel]
 
+def normalise_eeg(eeg_data):
+    '''Normalise each channel to have std of 1'''
+    return eeg_data / eeg_data.std(axis=1)[:, None]
+
+# -----------------------------------------------------------------------
+# Filterings 
+# -----------------------------------------------------------------------
 
 def filt(samples_buffer, lf=8, hf=30, nf=50, fs=250):
     '''Apply a bandpass and notch filter
@@ -101,13 +113,13 @@ def bandpass(samples_buffer: np.ndarray, lf=4, hf=30, fs=250, ftype:str='fir'):
         - lf: low frequency cuttoff
         - hf: high frequency cuttoff
         - fs: sampling frequency
-        - ftype: filter type
+        - ftype: filter type [fir, iir, butter]
     Returns:
-        - filtered data of shape (num_channels)(buffer_size)
+        - filtered data of shape (num_channels, buffer_size)
     '''
     # Check type
     try:
-        assert ftype in ['fir', 'iir'], 'You did not specify the type correctly'
+        assert ftype in ['fir', 'iir', 'butter'], 'You did not specify the type correctly'
     except Exception as e:
         raise
 
@@ -130,6 +142,11 @@ def bandpass(samples_buffer: np.ndarray, lf=4, hf=30, fs=250, ftype:str='fir'):
         )
 
         filtered_data = signal.lfilter(b_bp, [1], samples_buffer, axis=1)
+    
+    elif ftype == 'butter':
+        b, a = signal.butter(4, Wn=[lf, hf], btype='bandpass', fs=fs)
+
+        filtered_data = signal.filtfilt(b, a, samples_buffer, padlen=150)
     
     return filtered_data
 
@@ -162,6 +179,108 @@ def visualise_filter(filter_params, fs:int):
     plt.plot(w1, 20*np.log10(np.abs(h1)), 'b')
 
     plt.show()
+
+
+# -----------------------------------------------------------------------
+# Channels by name or by channel number
+# -----------------------------------------------------------------------
+def channel2name(channel_number):
+    mapping = {
+        0:  'Fz',    
+        1:  'C3', 
+        2:  'Cz', 
+        3:  'C4',
+        4:  'Pz', 
+        5:  'PO7', 
+        6:  'Oz', 
+        7:  'PO8',
+    }
+    return mapping[channel_number]
+
+def name2channel(name):
+    mapping = {
+        'Fz': 0,    
+        'C3': 1, 
+        'Cz': 2, 
+        'C4': 3,
+        'Pz': 4, 
+        'PO7': 5,
+        'Oz': 6, 
+        'PO8': 7,
+    }
+    return mapping[name]
+
+def get_channel(channel_name:str, eeg_data):
+    '''
+    Params:
+        - channel_name: One of [Fz, C3, Cz, C4, Pz, P07, Oz, PO8]
+        - eeg_data: shape (num_channels, num_samples)
+    Returns:
+        - single channel
+    '''
+    return eeg_data[name2channel[channel_name], :]
+
+
+# -----------------------------------------------------------------------
+# CSP (common spatial pattern) Related Methods
+# -----------------------------------------------------------------------
+class CSP:
+
+    def __init__(self, trials, cl_labs, nsamples, nchannels, W=None) -> None:
+        self.nsamples = nsamples
+        self.nchannels = nchannels
+        self.trials = trials
+
+        if W is None:
+            self.W = self.csp(trials[cl_labs[0]], trials[cl_labs[1]])
+        else:
+            self.W = W
+
+        self.trials_csp = {
+            cl_labs[0]: self.apply_mix(self.W, self.trials[cl_labs[0]]),
+            cl_labs[1]: self.apply_mix(self.W, self.trials[cl_labs[1]])
+        }
+
+    def get_csp_trials(self):
+        return self.trials_csp
+    
+    def get_W(self):
+        return self.W
+
+    def cov(self, trials):
+        ''' Calculate the covariance for each trial and return their average '''
+        ntrials = trials.shape[2]
+        covs = [ trials[:,:,i].dot(trials[:,:,i].T) / self.nsamples for i in range(ntrials) ]
+        return np.mean(covs, axis=0)
+
+    def whitening(self, sigma):
+        ''' Calculate a whitening matrix for covariance matrix sigma. '''
+        U, l, _ = linalg.svd(sigma)
+        return U.dot( np.diag(l ** -0.5) )
+
+    def csp(self, trials_r, trials_f):
+        '''
+        Calculate the CSP transformation matrix W.
+        arguments:
+            trials_r - Array (channels x samples x trials) containing right hand movement trials
+            trials_f - Array (channels x samples x trials) containing foot movement trials
+        returns:
+            Mixing matrix W
+        '''
+        cov_r = self.cov(trials_r)
+        cov_f = self.cov(trials_f)
+        P = self.whitening(cov_r + cov_f)
+        B, _, _ = linalg.svd( P.T.dot(cov_f).dot(P) )
+        W = P.dot(B)
+        return W
+
+    def apply_mix(self, W, trials):
+        ''' Apply a mixing matrix to each trial (basically multiply W with the EEG signal matrix)'''
+        ntrials = trials.shape[2]
+        trials_csp = np.zeros((self.nchannels, self.nsamples, ntrials))
+        for i in range(ntrials):
+            trials_csp[:,:,i] = W.T.dot(trials[:,:,i])
+        return trials_csp
 
 # -----------------------------------------------------------------------
 # MNE Preprocessing
